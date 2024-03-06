b70a4869156e5fc6162030645cedcc454faa33bf
raj-codinggit config --global user.name raj-coding1~ <rkgarai17@gmail.comgit config --global user.email rkgarai17@gmai>
2024-03-06 14:50:48 +0530

    commit

---
+++ project1.html/mixofhtmlandjs.html/object.js
@@ -0,0 +1,12 @@
+let group = {
+    name1: "rajkumar",
+    name2: "ajeet rav",
+    name3: "praphul",
+    attribute: {
+        roll: 51,
+        branch: "btech",
+
+    }
+
+}
+console.log(group.attribute.roll);
